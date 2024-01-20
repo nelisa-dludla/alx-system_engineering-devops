5e73560d83b0987e6fbd38e964d2deea29e5cfc2
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-01-20 02:47:18 +0200

    Updated task 1 solution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,16 +4,12 @@ package { 'python3':
   ensure   => '3.8.10',
 }
 
-package { 'python3-pip':
-  ensure   => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
 package { 'werkzeug':
-  ensure   => 'latest',
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
