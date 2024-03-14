c906da4f94c7870d1b0cec8f553cf21b4e7a81dd
Nelisa <nelisa.dludla@outlook.com>
2024-03-14 21:53:13 +0200

    Updated task 1 - Used puppet-lint to fix indenation

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Fixes "Too many files open" error for user Holberton
 
 exec { 'fix holberton user hard limit':
-  command  => 'sed -i "s/^holberton hard nofile.*/holberton hard nofile 10000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin:/bin',
+  command => 'sed -i "s/^holberton hard nofile.*/holberton hard nofile 10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin',
 }
 
 exec { 'fix holberton user soft limit':
-  command  => 'sed -i "s/^holberton soft nofile.*/holberton soft nofile 10000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin:/bin',
+  command => 'sed -i "s/^holberton soft nofile.*/holberton soft nofile 10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin',
 }
